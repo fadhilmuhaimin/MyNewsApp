8de5566f54345eedc50ec2bc258eb9e0790a15bc
Tomjerry12345 <andrialfiandi46@gmail.com>
2019-10-15 14:08:11 +0800

    test

--- app/src/main/java/com/dicoding/mynewsapp/OneFragment.java
+++ app/src/main/java/com/dicoding/mynewsapp/OneFragment.java
@@ -9,7 +9,7 @@ import android.view.ViewGroup;
 public class OneFragment extends Fragment {
 
     public OneFragment() {
-        // Required empty public constructord3f3f3
+        // Required empty public constructord3f3f3kjn l
     }
 
     @Override
